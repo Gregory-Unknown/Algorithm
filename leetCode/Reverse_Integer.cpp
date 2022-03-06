bc96323086159e2cdb7f8c3d139e5037bc7cbfa1
Gunshoot <grrisha2593@gmail.com>
2022-03-06 20:50:34 +0300

    Another one

---
+++ leetCode/Reverse_Integer.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stack>
+
+int reverse(int x) {
+    std::stack<int>st;
+    for (; x != 0; x /= 10) {
+        st.push(x % 10);
+    }
+    long res = 0;
+    for (long i = 1; !st.empty(); i *= 10)
+    {
+        res += st.top() * i;
+        st.pop();
+    }
+    if (res > INT_MAX || res < INT_MIN) return (0);
+    return (res);
+}
+
+int main()
+{
+    int num = 12334567890987654321;
+
+    std::cout << reverse(num) << std::endl;
+}
